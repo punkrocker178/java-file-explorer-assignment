88569c99e5b7d00cffb9f5fe7bb29120543e8f39
Steve Le <steve.le@contemi.com.vn>
2020-03-24 17:02:12 +0700

    Create basic view for folder & file explorer: - Added go back to previous directory - Added address bar - Directories and files are shown as Label

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,137 @@
 package sample;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
-public class Controller {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
+import java.util.Stack;
+
+public class Controller implements Initializable {
+
+    private static final String basePath = "/home/";
+    private static final int itemPerRow = 4;
+
+    public Stack<Node> nodes;
+    public Stack<String> paths;
+
+    public TextField addressBar;
+    public GridPane gridPane;
+    public Button backBtn;
 
     public void click() {
         System.out.println("Clicked");
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        nodes = new Stack<>();
+        paths = new Stack<>();
+        paths.add(basePath);
+        try {
+            traverse(Paths.get(basePath));
+            showDirectories();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public EventHandler<MouseEvent> clickLabel() {
+        return event -> {
+            if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
+                String directoryName = ((Label)event.getSource()).getText();
+                try {
+                    traverse(Paths.get(paths.peek() , directoryName));
+                    showDirectories();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+    public void createLabel(Path file) {
+        Label label = new Label(file.getFileName().toString());
+        label.addEventHandler(MouseEvent.MOUSE_CLICKED, clickLabel());
+        nodes.add(label);
+    }
+
+    public void showDirectories() {
+        int numRow = (int) Math.ceil(nodes.size() / 4.0);
+        for (int i = 0; i < numRow ; i++) {
+            for (int j = 0; j < itemPerRow; j++) {
+                if (nodes.size() > 0) {
+                    gridPane.add(nodes.pop(), j, i);
+                }
+            }
+        }
+    }
+
+    public void traverse(Path path) throws IOException {
+
+        gridPane.getChildren().clear();
+        DirectoryStream<Path> stream = Files.newDirectoryStream(path);
+
+        if (Files.isDirectory(path)) {
+
+            if (paths.size() > 5) {
+                paths.remove(paths.size() -1);
+            }
+
+            System.out.println("\nDirectory " + path.getFileName() + "\t Parent:" + path.getParent());
+            paths.push(getPath(path));
+            addressBar.setText(getPath(path));
+        }
+
+        for (Path file: stream) {
+
+//                if (Files.isRegularFile(file)) {
+//                    System.out.print(file.getFileName() + "\t");
+//                }
+
+//                if (Files.isDirectory(file)) {
+//                    System.out.print(file.getFileName() + "\t");
+//                    traverse(file.toAbsolutePath());
+//                }
+            System.out.print(file.getFileName() + "\t");
+            createLabel(file);
+//
+        }
+
+    }
+
+    public String getPath(Path path) {
+        return Paths.get(path.getParent().toString(), path.getFileName().toString()).toString();
+    }
+
+    public void goBack() {
+        try {
+            if (paths.size() > 1) {
+                paths.pop();
+                traverse(Paths.get(paths.pop()));
+            }
+            showDirectories();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,42 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.nio.file.*;
 
 public class Main extends Application {
 
-    private static final String basePath = "/home/stevele/Desktop/NNLT";
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+        primaryStage.setTitle("File Explorer");
+        primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
-
-        try {
-            traverse(Paths.get(basePath));
-        } catch (IOException | DirectoryIteratorException x) {
-            System.err.println(x);
-        }
-    }
-
-    public static void traverse(Path path) throws IOException {
-        if (Files.isDirectory(path)) {
-            DirectoryStream<Path> stream = Files.newDirectoryStream(path);
-            System.out.println("\nDirectory " + path.getFileName() + "\t Parent:" + path.getParent());
-            for (Path file: stream) {
-                if (Files.isRegularFile(file)) {
-                    System.out.print(file.getFileName() + "\t");
-                } else {
-                    traverse(file.toAbsolutePath());
-                }
-            }
-        }
     }
 
 
